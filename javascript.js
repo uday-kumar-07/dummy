b627558c0f668acd1675deb9f12328fbab0e3e96
Uday Kumar <136891992+uday-kumar-07@users.noreply.github.com>
2023-10-15 00:04:11 +0530

    Adding to GitHub

---
+++ javascript.js
@@ -0,0 +1,16 @@
+const changeColorButton = document.getElementById("changeColorButton");
+const content = document.querySelector(".content");
+
+changeColorButton.addEventListener("click", () => {
+    const randomColor = getRandomColor();
+    content.style.backgroundColor = randomColor;
+});
+
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
